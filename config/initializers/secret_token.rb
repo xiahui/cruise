a3080c14bcee35bf09636518a6385647fa9f1009
xiahui <xiahui@tanei.com>
2011-07-19 09:14:43 +0800

    initialize cruise project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cruise::Application.config.secret_token = '6e3e2cb65473eda00f0aeff93be413d5f4018d8a576f9de1543d5ae7dd0c7cb66e2a52c9fcb4a3c80305987df2b9decf6061398a80611d61f0fdc3e8f829ad74'
